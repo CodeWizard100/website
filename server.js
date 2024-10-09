ace6ba257a09492bcfdcd8a4ae05d934f0ea450c
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-10-09 10:15:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Use the PORT environment variable or default to 3000
 
+
 app.get('/', (req, res) => {
   const secretValue = process.env.test; // Get the value of the 'test' secret
   res.send(`Hi! The secret value is: ${secretValue}`); // Respond with the secret value
