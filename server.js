badc2d70741f5600fead1b1bfb87ec551d4b5bc7
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-10-11 17:49:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -147,6 +147,30 @@ app.post('/generatenewtoken', async (req, res) => {
     }
 });
 
+app.post('/getmoney', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        // Check if the user exists
+        const response = await axios.get(`${process.env.link}/Players/${username}.json`);
+
+        // If the user does not exist
+        if (response.data === null) {
+            return res.status(400).json({ message: 'User does not exist!' });
+        }
+
+        // Check if the password matches
+        if (response.data.password !== password) {
+            return res.status(400).json({ message: 'Incorrect password!' });
+        }
+
+        // Successful login
+        return res.status(200).json({ message: response.data.money});
+    } catch (error) {
+        console.error('Error logging in user:', error);
+        return res.status(500).json({ message: 'Error getting money in user!' });
+    }
+});
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
